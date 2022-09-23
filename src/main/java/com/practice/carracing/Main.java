7b35b661945ca318237756c1a0d6338840d4c1bd
sangjaeoh <sangjaeoh91@gmail.com>
2022-09-23 21:57:01 +0900

    자동차 경주 게임

---
+++ src/main/java/com/practice/carracing/Main.java
@@ -0,0 +1,10 @@
+package com.practice.carracing;
+
+public class Main {
+
+    public static void maim(String[] args) {
+        // TODO document why this method is empty
+
+    }
+
+}
---
+++ src/test/java/com/practice/carracing/MainTest.java
@@ -0,0 +1,14 @@
+package com.practice.carracing;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+
+    @Test
+    void maim() {
+
+    }
+}
